10f0db4e5b0d5bec0773776ae6bd66193645f6c2
michellemarionette <lucaayscough@gmail.com>
2022-10-05 18:27:54 +0100

    Added comments.

--- Assignment_1/Assignment_1.cpp
+++ Assignment_1/Assignment_1.cpp
@@ -1,68 +1,68 @@
-// this is used to output messages to the console
+// This is used to output messages to the console.
 #include <iostream>
 
-// this is used to seed the rand() function
+// This is used to seed the rand() function.
 #include <time.h>
 
 int main()
 {
-    // seeds the rand() function that it will generate random values
+    // Seeds the rand() function that it will generate random values.
     srand(time(NULL));
     
-    // variables storing the upper and lower frequency boundaries
+    // Variables storing the upper and lower frequency boundaries.
     int frequencyUpperLimit = 880;
     int frequencyLowerLimit = 220;
     
-    // variables containing values used in the program for readibility
+    // Variables containing values used in the program for readibility.
     int randomNumberModulo = 10000;
     int containerSize = 50;
     int numOscillators = 3;
     
-    // pointers to arrays which will later store data
+    // Pointers to arrays which will later store data.
     float* volumes = new float[containerSize];
     float* frequencies = new float[containerSize];
     int* oscillatorType = new int[containerSize];
     
-    // variables used determine highest and lowest values in the array
+    // Variables used to determine the highest and lowest values in the array,
     // they are oppositely set to the lowest and highest value so that as
-    // the algorithm progresses the values will be respectively raised or lowered
+    // the algorithm progresses the values will be respectively raised or lowered.
     int highestFrequency = frequencyLowerLimit;
     int lowestFrequency = frequencyUpperLimit;
     
     for (int i = 0; i < containerSize; i++)
     {
-        // generates random value between 0 and 1 and stores it
+        // Generates random value between 0 and 1 and stores it.
         float randomVolume = (rand() % randomNumberModulo) * 1.0 / randomNumberModulo;
         
-        // generates random value between lower and upper frequnecy boundaries (inclusive)
+        // Generates random value between lower and upper frequnecy boundaries (inclusive);
         // first a random number is generated between 0 and the upper limit minus the lower limit
-        // and then the lower limit is added back on to the randomly generated value
+        // and then the lower limit is added back on to the randomly generated value.
         float randomFrequency = frequencyLowerLimit + (rand() % (frequencyUpperLimit - frequencyLowerLimit + 1));
         
-        // generated random value between 0 and 3 (inclusive)
+        // Generated random value between 0 and 3 (inclusive).
         int randomOscillator = rand() % numOscillators + 1;
         
-        // checks to see if the random frequency is lower than what is already stored as the lowest value
-        // and if true stores it
+        // Checks to see if the random frequency is lower than what is already stored as the lowest value
+        // and if true stores it.
         if (randomFrequency < lowestFrequency)
         {
             lowestFrequency = randomFrequency;
         }
         
-        // checks to see if the random frequency is higher than what is already stored as the lowest value
-        // and if true stores it
+        // Checks to see if the random frequency is higher than what is already stored as the lowest value
+        // and if true stores it.
         if (randomFrequency > highestFrequency)
         {
             highestFrequency = randomFrequency;
         }
         
-        // stores the generated values inside the arrays
+        // Stores the generated values inside the arrays.
         volumes[i] = randomVolume;
         frequencies[i] = randomFrequency;
         oscillatorType[i] = randomOscillator;
     }
     
-    // prints to the console the highest and lowest values
+    // Prints to the console the highest and lowest values.
     std::cout << "Highest frequency is: " << highestFrequency << std::endl;
     std::cout << "Lowest frequency is: " << lowestFrequency << std::endl;
     
--- Assignment_1/Assignment_2.cpp
+++ Assignment_1/Assignment_2.cpp
@@ -1,23 +1,34 @@
+// Console input and output functions.
 #include <iostream>
+// Used for the power function.
 #include <cmath>
 
 
-/// asks user to input a midi note between the values of 0 and 127
+/// Asks user to input a midi note between the values of 0 and 127.
 float getMidiNote()
 {
+    // Switch used to check if the value inputted by the user is in range.
     bool _isNoteInRange = false;
+    
+    // Variable for midi note value.
     float _midiNote = 0;
     
+    // Loop in which the user is asked to input a midi note value.
+    // The loop will run until the _isNoteInRange variable is set to true.
     while (!_isNoteInRange)
     {
         std::cout << "Input midi value: " << std::endl;
         std::cin >> _midiNote;
         
+        // Checks if the value entered is in the range of 0-127.
+        // If true _isNoteInRange is switched and loop will be exited after code is run.
         if (_midiNote >= 0 && _midiNote <= 127)
         {
             _isNoteInRange = true;
         }
         
+        
+        // If false error message will be output and loop will run again.
         else
         {
             std::cout << "Value must be between 0 and 127." << std::endl;
@@ -27,11 +38,14 @@ float getMidiNote()
     return _midiNote;
 }
 
-/// takes as input a midi note and converts it into  the frequency domain
+/// Takes as input a midi note and converts it into the frequency domain.
 float midiToFrequency(float _midiNote)
 {
+    // Variables holding values for conversion equation.
     double _base = 2;
     double _exponent = (_midiNote - 69.0) / 12.0;
+    
+    // Caculates the frequancy based on the inputted midi.
     float _frequency = 440 * pow(_base, _exponent);
     
     return _frequency;
@@ -39,6 +53,7 @@ float midiToFrequency(float _midiNote)
 
 void printFirstEightHarmonics(float _frequency)
 {
+    // Loop which runs eight times and prints to the console the first eight harmonics of a given frequency.
     for(int i = 0; i < 8; i++)
     {
         std::cout << "Harmonic " << i + 1 << ": " << (int)_frequency * (i + 1) << " Hz" << std::endl;
@@ -48,11 +63,16 @@ void printFirstEightHarmonics(float _frequency)
 
 int main()
 {
+    // Gets the user to input midi note and stores it.
     float midiNote = getMidiNote();
+    
+    // Converts user inputted midi to its corresponding frequency.
     float frequency = midiToFrequency(midiNote);
     
-    std::cout << "Note " << midiNote << " corresponds to a frequnecy of " << (int)frequency << std::endl;
+    // Prints to the console the midi note value and its corresponding frequency.
+    std::cout << "Note " << midiNote << " corresponds to a frequnecy of " << (int)frequency << " Hz." << std::endl;
 
+    // Prints to the console the first eight harmonics of the user given frequency.
     printFirstEightHarmonics(frequency);
     
     return 0;
